b39c13d5fc661667c0fb255567978c77beb7406d
Chris Tranel <tranel@earthlinginteractive.com>
2023-03-24 12:41:11 -0500

    OWORK-508: WORK Translations Import

---
+++ lang/nl/certificateelement_programs.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of the tool_certificate plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Admin tool certificate element programs
+ *
+ * @package    certificateelement_programs
+ * @copyright  2023 Open LMS (https://www.openlms.net/)
+ * @author     Chris Tranel
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Programmaveld';
+$string['privacy:metadata'] = 'De plugin van programmavelden slaat geen persoonlijke gegevens op.';
+$string['programfield'] = 'Programmaveld';
+$string['programfield_help'] = 'Dit is het programmaveld dat wordt weergegeven in de PDF.';
