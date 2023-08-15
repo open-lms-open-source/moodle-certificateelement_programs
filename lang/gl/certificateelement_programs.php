52f39b9689879d1644425a650247b6b3e56c165f
Farhan Karmali <farhan.karmali@openlms.net>
2023-08-15 22:55:34 +0300

    OWORK-644 Add translations for eu and gl

---
+++ lang/gl/certificateelement_programs.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of the tool_certificate plugin for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'certificateelement_programs', language 'gl'.
+ *
+ * @package    certificateelement_programs
+ * @copyright  2022 Open LMS (https://www.openlms.net/)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Campo do programa';
+$string['privacy:metadata'] = 'O complemento de campos do programa non almacena datos persoais.';
+$string['programfield'] = 'Campo do programa';
+$string['programfield_help'] = 'Este é o campo do programa que se amosará no PDF.';
